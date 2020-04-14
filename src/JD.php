368e697bbc08c292c9304e4a52714026d41f92db
scpzc <335760480@qq.com>
2020-04-14 17:00:11 +0800

    初始化

--- src/JD.php
+++ src/JD.php
@@ -3,18 +3,18 @@
  * 京东接口包装.
  */
 
-namespace hVenus\JingdongAPI;
+namespace scpzc\JingdongAPI;
 
-use hVenus\JingdongAPI\API\ACCOUNT;
-use hVenus\JingdongAPI\API\ADDRESS;
-use hVenus\JingdongAPI\API\CATEGORY;
-use hVenus\JingdongAPI\API\DSP;
-use hVenus\JingdongAPI\API\ECLP;
-use hVenus\JingdongAPI\API\EXPRESS;
-use hVenus\JingdongAPI\API\Helper;
-use hVenus\JingdongAPI\API\ORDER;
-use hVenus\JingdongAPI\API\SHOP;
-use hVenus\JingdongAPI\Core\BaseClass;
+use scpzc\JingdongAPI\API\ACCOUNT;
+use scpzc\JingdongAPI\API\ADDRESS;
+use scpzc\JingdongAPI\API\CATEGORY;
+use scpzc\JingdongAPI\API\DSP;
+use scpzc\JingdongAPI\API\ECLP;
+use scpzc\JingdongAPI\API\EXPRESS;
+use scpzc\JingdongAPI\API\Helper;
+use scpzc\JingdongAPI\API\ORDER;
+use scpzc\JingdongAPI\API\SHOP;
+use scpzc\JingdongAPI\Core\BaseClass;
 
 class JD extends BaseClass
 {
