eddab3512c0024d9d342e61bb5440748990e5256
scpzc <335760480@qq.com>
2020-04-14 15:32:27 +0800

    init
    Signed-off-by: scpzc <335760480@qq.com>

---
+++ JD.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace scpzc\JingdongAPI;
+
+
+class JD
+{
+    public function test(){
+        echo 1;exit;
+        $c = new JdClient();
+        $c->appKey = "26EAC2509056EB38FB623D9A49296D2C";
+        $c->appSecret = "1abdc5a97ecb4594ab7b772296bcfbbd";
+        $c->accessToken = "1f1d3048-220a-484d-ad93-f3808d9aacc1";
+        $c->serverUrl = "http://gw.api.360buy.net/routerjson";
+        $req = new JingdongSetListMapRequest;
+        $req->putOtherTextParam("cid", "1");
+        $req->putOtherTextParam("value_id", "1");
+        $req->putOtherTextParam("source", "1");
+        $req->putOtherTextParam("ip", "1");
+        $resp = $c->execute($req, $c->accessToken);
+        print(json_encode($resp));
+    }
+}
